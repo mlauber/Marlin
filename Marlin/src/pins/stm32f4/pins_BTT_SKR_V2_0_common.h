1bfb14e590a55651b5a4b5cf4be9425c6d5538fb
mlauber <mattrlauber@gmail.com>
2022-01-10 14:10:18 -0500

    pins

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
@@ -309,16 +309,16 @@
   #define X_SERIAL_TX_PIN                   PE0
   #define X_SERIAL_RX_PIN        X_SERIAL_TX_PIN
 
-  #define Y_SERIAL_TX_PIN                   PD3
+  #define Y_SERIAL_TX_PIN                   PA4
   #define Y_SERIAL_RX_PIN        Y_SERIAL_TX_PIN
 
-  #define Z_SERIAL_TX_PIN                   PD0
+  #define Z_SERIAL_TX_PIN                   PA5
   #define Z_SERIAL_RX_PIN        Z_SERIAL_TX_PIN
 
-  #define E0_SERIAL_TX_PIN                  PC6
+  #define E0_SERIAL_TX_PIN                  PE11
   #define E0_SERIAL_RX_PIN      E0_SERIAL_TX_PIN
 
-  #define E1_SERIAL_TX_PIN                  PD12
+  #define E1_SERIAL_TX_PIN                  PE13
   #define E1_SERIAL_RX_PIN      E1_SERIAL_TX_PIN
 
   // Reduce baud rate to improve software serial reliability
