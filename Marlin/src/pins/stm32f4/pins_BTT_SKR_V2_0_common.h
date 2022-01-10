64ddd4104e1cebf3c2c9fae48628adcc6a740536
mlauber <mattrlauber@gmail.com>
2022-01-10 15:02:58 -0500

    pins update

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
@@ -497,12 +497,12 @@
     #endif // !FYSETC_MINI_12864
 
     #if IS_ULTIPANEL
-      #define LCD_PINS_D5            EXP1_05_PIN
-      #define LCD_PINS_D6            EXP1_04_PIN
-      #define LCD_PINS_D7            EXP1_03_PIN
+      //#define LCD_PINS_D5            EXP1_05_PIN
+      //#define LCD_PINS_D6            EXP1_04_PIN
+      //#define LCD_PINS_D7            EXP1_03_PIN
 
       #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
+        //#define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
       #endif
 
     #endif
